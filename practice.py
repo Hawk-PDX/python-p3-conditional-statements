00715e518015fe531a4d72be3064937330696ced
Garrett <hawkpdx@icloud.com>
2025-01-11 18:15:16 -0800

    @ last deliv

---
+++ practice.py
@@ -0,0 +1,17 @@
+pet_info = [
+    {
+        "name": "Lion", 
+        "age": 5, 
+        "weight": 250
+    },
+    {
+        "name": "Tiger",
+        "age": 3,
+        "weight": 180
+    },
+    {
+        "name": "Bear",
+        "age": 2,
+        "weight": 150
+    }
+]
